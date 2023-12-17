9e30006fe3d59b96d04f9df2512ee274d3d3017b
maxinelobato <maxinelobato89@hotmail.com>
2023-12-17 16:52:58 -0300

    canges

--- src/app/components/GoogleBusiness/index.tsx
+++ src/app/components/GoogleBusiness/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import './swiper.css'
+import "./swiper.css";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -14,11 +14,36 @@ import client2 from "/public/assets/client2.jpeg";
 import client3 from "/public/assets/client3.jpeg";
 import client4 from "/public/assets/client4.jpeg";
 import client5 from "/public/assets/client5.jpeg";
+import Script from "next/script";
 
 export default function GoogleBusiness() {
   return (
     <>
-        <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6">
+      <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6">
+        <div className="mx-auto max-w-screen-xl">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="uppercase font-sans font-medium text-3xl tracking-wide text-brandSecondary sm:text-4xl sm:leading-none">
+              O que os clientes falam {""}
+              <span className="font-black font-sans text-transparent bg-clip-text bg-brandSecondary">
+                sobre a Dra. Jamile Gomes
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="mx-auto max-w-6xl px-4 pt-8 sm:px-6">
+          <Script
+            strategy="afterInteractive"
+            src="https://static.elfsight.com/platform/platform.js"
+            data-use-service-core
+            defer
+          ></Script>
+          <div
+            className="elfsight-app-3f69b63e-055a-45cd-8844-18ae4b4c401d"
+            data-elfsight-app-lazy
+          ></div>
+        </div>
+      </div>
+      {/* <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6">
           <div className="mx-auto max-w-screen-xl">
             <div className="mx-auto max-w-3xl text-center">
               <h1 className="uppercase font-sans font-medium text-3xl tracking-wide text-brandSecondary sm:text-4xl sm:leading-none">
@@ -111,7 +136,7 @@ export default function GoogleBusiness() {
               </SwiperSlide>
             </Swiper>
           </div>
-        </div>
+        </div> */}
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,31 @@
-import { Suspense } from 'react'
-import './globals.css'
-import { Montserrat } from 'next/font/google'
-import Script from 'next/script'
+import { Suspense } from "react";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import Script from "next/script";
 
 const montserrat = Montserrat({
-  subsets: ['latin', 'cyrillic', 'cyrillic-ext', 'latin-ext', 'vietnamese'],
-  weight: ['100', '200', '300', '400']
-})
+  subsets: ["latin", "cyrillic", "cyrillic-ext", "latin-ext", "vietnamese"],
+  weight: ["100", "200", "300", "400"],
+});
 
 export const metadata = {
   title:
-    'Jamile Gomes - Advocacia e Consultoria Jurídica em Cidadania e Nacionalidade Portuguesa',
+    "Jamile Gomes - Advocacia e Consultoria Jurídica em Cidadania e Nacionalidade Portuguesa",
   description:
-    'Advogada Especialista em Cidania e Nacionalidade para Brasileiros em Portugal'
-}
+    "Advogada Especialista em Cidania e Nacionalidade para Brasileiros em Portugal",
+};
 
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='pt-BR'>
+    <html lang="pt-BR">
       <Suspense>
         <Script
-          id='gtag-base'
-          strategy='afterInteractive'
+          id="gtag-base"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -33,7 +33,7 @@ export default function RootLayout ({
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', 'GTM-TTQ336T');
-          `
+          `,
           }}
         ></Script>
       </Suspense>
@@ -43,13 +43,13 @@ export default function RootLayout ({
           <noscript>
             <iframe
               src={`https://www.googletagmanager.com/ns.html?id=GTM-TTQ336T`}
-              height='0'
-              width='0'
-              style={{ display: 'none', visibility: 'hidden' }}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
             />
           </noscript>
         </Suspense>
       </body>
     </html>
-  )
+  );
 }

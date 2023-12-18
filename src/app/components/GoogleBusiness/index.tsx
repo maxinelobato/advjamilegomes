b28498bf62e22fe2351647315095e6b40738af1f
maxinelobato <maxinelobato89@hotmail.com>
2023-12-17 22:08:23 -0300

    changes

--- src/app/components/GoogleBusiness/index.tsx
+++ src/app/components/GoogleBusiness/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./swiper.css";
@@ -17,7 +17,7 @@ import client5 from "/public/assets/client5.jpeg";
 // import Script from "next/script";
 
 // import dynamic from 'next/dynamic'
- 
+
 // const DynamicGoogleElfsightWidget = dynamic(() => import('../GoogleElfsightWidget'), {
 //   ssr: false,
 // })
@@ -25,7 +25,6 @@ import client5 from "/public/assets/client5.jpeg";
 // import { ElfsightWidget } from "react-elfsight-widget";
 
 export default function GoogleBusiness() {
-
   return (
     <>
       {/* <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6">
@@ -44,103 +43,121 @@ export default function GoogleBusiness() {
         </div>
       </div> */}
       <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6">
-          <div className="mx-auto max-w-screen-xl">
-            <div className="mx-auto max-w-3xl text-center">
-              <h1 className="uppercase font-sans font-medium text-3xl tracking-wide text-brandSecondary sm:text-4xl sm:leading-none">
-                  O que os clientes falam {""}
-                  <span className="font-black font-sans text-transparent bg-clip-text bg-brandSecondary">
-                    sobre a Dra. Jamile Gomes
-                  </span>
-                </h1>
-            </div>
-            <div className="mt-10 flex flex-wrap items-center justify-center gap-4 xl:justify-center">
-              <Image
-                src="/assets/google.png"
-                alt="Imagem"
-                width={300}
-                height={300}
-                rel="preload"
-                className="w-28 h-28 drop-shadow-lg"
-              />
-            </div>
+        <div className="mx-auto max-w-screen-xl">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="uppercase font-sans font-medium text-3xl tracking-wide text-brandSecondary sm:text-4xl sm:leading-none">
+              O que os clientes falam {""}
+              <span className="font-black font-sans text-transparent bg-clip-text bg-brandSecondary">
+                sobre a Dra. Jamile Gomes
+              </span>
+            </h1>
           </div>
-
-          <div className="mx-auto max-w-4xl px-4 pt-8 sm:px-6">
-            <Swiper
-              navigation={true}
-              autoplay={{
-                delay: 2200,
-                disableOnInteraction: false,
-              }}
-              modules={[Navigation, Autoplay]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <blockquote className="items-center justify-center sm:p-8 lg:p-12">
-                  <Image
-                    src={client1}
-                    alt="Imagem"
-                    rel="preload"
-                    layout="responsive"
-                    className="cursor-pointer rounded-2xl"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </blockquote>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <blockquote className="items-center justify-center sm:p-8 lg:p-12">
-                  <Image
-                    src={client2}
-                    alt="Imagem"
-                    rel="preload"
-                    className="cursor-pointer rounded-2xl"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </blockquote>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <blockquote className="items-center justify-center sm:p-8 lg:p-12">
-                  <Image
-                    src={client3}
-                    alt="Imagem"
-                    rel="preload"
-                    layout="responsive"
-                    className="cursor-pointer rounded-2xl"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </blockquote>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <blockquote className="items-center justify-center sm:p-8 lg:p-12">
-                  <Image
-                    src={client4}
-                    alt="Imagem"
-                    rel="preload"
-                    layout="responsive"
-                    className="cursor-pointer rounded-2xl"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </blockquote>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <blockquote className="items-center justify-center sm:p-8 lg:p-12">
-                  <Image
-                    src={client5}
-                    alt="Imagem"
-                    rel="preload"
-                    layout="responsive"
-                    className="cursor-pointer rounded-2xl"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </blockquote>
-              </SwiperSlide>
-            </Swiper>
+          <div className="mt-10 flex flex-wrap items-center justify-center gap-4 xl:justify-center">
+            <Image
+              src="/assets/google.png"
+              alt="Imagem"
+              width={300}
+              height={300}
+              rel="preload"
+              className="w-28 h-28 drop-shadow-lg"
+            />
           </div>
         </div>
+
+        <div className="mx-auto max-w-4xl px-4 pt-8 sm:px-6">
+          <Swiper
+            navigation={true}
+            autoplay={{
+              delay: 2200,
+              disableOnInteraction: false,
+            }}
+            modules={[Navigation, Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <blockquote className="items-center justify-center sm:p-8 lg:p-12">
+                <Image
+                  src={client1}
+                  alt="Imagem"
+                  rel="preload"
+                  layout="responsive"
+                  className="cursor-pointer rounded-2xl"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+              </blockquote>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <blockquote className="items-center justify-center sm:p-8 lg:p-12">
+                <Image
+                  src={client2}
+                  alt="Imagem"
+                  rel="preload"
+                  className="cursor-pointer rounded-2xl"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+              </blockquote>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <blockquote className="items-center justify-center sm:p-8 lg:p-12">
+                <Image
+                  src={client3}
+                  alt="Imagem"
+                  rel="preload"
+                  className="cursor-pointer rounded-2xl"
+                  width={500}
+                  height={500}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  priority
+                />
+              </blockquote>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <blockquote className="items-center justify-center sm:p-8 lg:p-12">
+                <Image
+                  src={client4}
+                  alt="Imagem"
+                  rel="preload"
+                  className="cursor-pointer rounded-2xl"
+                  width={500}
+                  height={500}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  priority
+                />
+              </blockquote>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <blockquote className="items-center justify-center sm:p-8 lg:p-12">
+                <Image
+                  src={client5}
+                  alt="Imagem"
+                  rel="preload"
+                  className="cursor-pointer rounded-2xl"
+                  width={500}
+                  height={500}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  priority
+                />
+              </blockquote>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </>
   );
 }

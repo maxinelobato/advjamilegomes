725e520ce09d3bb5a974aa36aa0a80ae80c27069
maxinelobato <maxinelobato89@hotmail.com>
2024-08-15 22:48:19 -0300

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    domains: [
-       'advogadajamilegomes.com',
-    ],
+    images: {
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        formats: ['image/avif', 'image/webp'],
+        minimumCacheTTL: 60,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
